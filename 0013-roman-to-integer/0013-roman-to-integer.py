6b35b3346ecd662154a69aa05979631453d0a05b
Raghunandan-Kotary <kotaryraghunandan@gmail.com>
2026-08-10 00:14:33 +0530

    Time: 5 ms (82.14%), Space: 12.4 MB (57.46%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        value={
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        res=0
+        for i in range(0,len(s)-1):
+            if value[s[i]]<value[s[i+1]]:
+                res-=value[s[i]]
+            else:
+                res+=value[s[i]]
+        res+=value[s[len(s)-1]]
+        return res
+
+        
